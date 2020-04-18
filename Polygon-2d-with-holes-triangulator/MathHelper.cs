2c2901d937042107b417470021525606644dccfc
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-04-18 18:47:00 +0200

    Added projects and code

---
+++ Polygon-2d-with-holes-triangulator/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace PolygonWithHolesTriangulator
+{
+    internal static class MathHelper
+    {
+        public static float Max(float d1, float d2)
+        {
+            if (d1 >= d2) return d1;
+            else return d2;
+        }
+
+        public static float Min(float d1, float d2)
+        {
+            if (d1 <= d2) return d1;
+            else return d2;
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            var m = Max(value, min);
+            return Min(m, max);
+        }
+    }
+}
